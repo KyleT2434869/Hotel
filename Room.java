3c901f30147ff379c571c760f360e68df84882bc
Kyle T <khoavominh.truong.900@my.csun.edu>
2025-07-16 15:58:45 -0700

    Added a setValues method in Room.java, and called it in Hotel.java

--- Room.java
+++ Room.java
@@ -4,4 +4,10 @@ public class Room {
     private int price;
     boolean smoking;
     boolean pets;
+
+    public void setValues(int val1, int val2, int val3) {
+        this.amount = val1;
+        this.capacity = val2;
+        this.price = val3;
+    }
 }
