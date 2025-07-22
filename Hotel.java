0aba570a8738aa23e0ec024c36e46d640a4c7f56
pushpak3205 <pushpak-rasikbhai.khunt.333@my.csun.edu>
2025-07-21 20:22:39 -0700

    Added print statemet

--- Hotel.java
+++ Hotel.java
@@ -7,5 +7,7 @@ public class Hotel {
         standard.setValues(100, 1, 250);
         deluxe.setValues(50, 2, 500);
         suite.setValues(5, 5, 1000);
+
+         System.out.println(" Hotel configuration updated by pushpak");
     }
 }
