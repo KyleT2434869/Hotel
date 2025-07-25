a5a199f19f4f5c9809b92226df8207c5a7201b51
pushpak3205 <pushpak-rasikbhai.khunt.333@my.csun.edu>
2025-07-25 11:39:21 -0700

    creted scene 1 and scene1.java

--- src/App.java
+++ src/App.java
@@ -1,4 +1,17 @@
-public class App {
+import javafx.application.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+public class App extends Application { 
     public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+
+        stage.show();
     }
 }
