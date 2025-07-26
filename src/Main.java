45ee2aa9b97129fa27b8a3a71fd00d2ced61b2fd
Xiomara Gamez <xiomaragamez2003@gmail.com>
2025-07-26 00:38:20 -0700

    Add roomRerservations method

--- src/Main.java
+++ src/Main.java
@@ -17,4 +17,23 @@ public class Main extends Application {
 
         stage.show();
     }
+
+    public void roomReservation() {
+        String name;
+        String email;
+        String roomType;
+        int id;
+        int partySize;
+        Hotel room;
+
+        Guest guest = new Guest(name, email, id, partySize);
+
+        if(roomType.equalsIgnoreCase("Standard")) {
+            room = new Standard();
+        } else if (roomType.equalsIgnoreCase("Deluxe")) {
+            room = new Deluxe();
+        } else if (roomType.equalsIgnoreCase("Suite")) {
+            room = new Suite();
+        } else { System.out.println("Invalid room type"); }
+    }
 }
