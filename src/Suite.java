67bf6539cd90b9d54b03b86670c449ae07ff33dd
Xiomara Gamez <xiomaragamez2003@gmail.com>
2025-07-25 23:46:57 -0700

    Add Suite class

---
+++ src/Suite.java
@@ -0,0 +1,9 @@
+public class Suite extends Hotel {
+    String roomType;
+    double price;
+
+    Suite(String roomType, double price) {
+        roomType = "Standard";
+        price = 200.00;
+    }
+}
