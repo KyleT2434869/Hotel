8e37944097ea3e521796d051726ebfb9827aa7e6
Xiomara Gamez <xiomaragamez2003@gmail.com>
2025-07-25 21:04:22 -0700

    Add Guest class

--- src/Guest.java
+++ src/Guest.java
@@ -1,9 +1,39 @@
+/*
+UML Diagram
+
+Guest
+
+- name : String
+- email : String
+- id : int
+- partySize : int
+
++ Guest(name : String, email : String, partySize : int)
++ getName() : String : final
++ getEmail() : String : final
++ getPartSize() : int : final
++ printInfo() : void
+ */
+
 public class Guest {
     private String name;
-    private int party;
+    private String email;
+    private int id;
+    private int partySize;
 
-    public void Guest(String s, int num) {
-        this.name = s;
-        this.party = num;
+    Guest(String name, String email,int id, int partySize) {
+        this.name = name;
+        this.email = email;
+        this.id = id;
+        this.partySize = partySize;
+    }
+    public final String getName() {
+        return name;
+    }
+    public final String getEmail() {
+        return email;
+    }
+    public final int getId() {
+        return id;
     }
 }
