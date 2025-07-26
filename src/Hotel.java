5b9a28a888001f14bd29d54f7e3db0583d6a070c
Xiomara Gamez <xiomaragamez2003@gmail.com>
2025-07-25 21:55:08 -0700

    Updated Hotel class

--- src/Hotel.java
+++ src/Hotel.java
@@ -1,3 +1,31 @@
+/*
+UML Diagram
+
+Hotel
+
+- guest : Guest
+- roomType : String
+- price : double
+
++ Hotel(guest : Guest, roomType : String, price : double)
++ getRoomType() : String
++ getPrice() : double
+ */
+
 public class Hotel {
-    
+    Guest guest;
+    String roomType;
+    double price;
+
+    Hotel(Guest guest, String roomType, double price) {
+        this.guest = guest;
+        this.roomType = roomType;
+        this.price = price;
+    }
+    public String getRoomType() {
+        return roomType;
+    }
+    public double getPrice() {
+        return price;
+    }
 }
