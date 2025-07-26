d6163733a3cb6a1116b5532684212d409b4316d5
Xiomara Gamez <xiomaragamez2003@gmail.com>
2025-07-25 23:45:21 -0700

    Add Deluxe class

---
+++ src/Deluxe.java
@@ -0,0 +1,9 @@
+public class Deluxe extends Hotel{
+    String roomType;
+    double price;
+
+    Deluxe(String roomType, double price) {
+        roomType = "Standard";
+        price = 150.00;
+    }
+}
