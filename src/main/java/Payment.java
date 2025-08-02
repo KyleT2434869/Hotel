c11b485eeaacf52f08c80551229c11e4a0e5a996
Xiomara Gamez <xiomaragamez2003@gmail.com>
2025-08-02 14:23:14 -0700

    add variables & get methods

--- src/main/java/Payment.java
+++ src/main/java/Payment.java
@@ -1,5 +1,27 @@
+import java.time.LocalDate;
+
 public class Payment {
-    private String cardType;
-    private int cardNum;
-    private String bank;
+    private String paymentID;
+    private String method;
+    private double amount;
+    private LocalDate date;
+
+    public Payment(String paymentID, String method, double amount, LocalDate date) {
+        this.paymentID = paymentID;
+        this.method = method;
+        this.amount = amount;
+        this.date = date;
+    }
+    public String getPaymentID() {
+        return paymentID;
+    }
+    public String getMethod() {
+        return method;
+    }
+    public double getAmount() {
+        return amount;
+    }
+    public LocalDate getDate() {
+        return date;
+    }
 }
