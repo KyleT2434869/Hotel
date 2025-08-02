072eb9a899ee3b31e2206b63ae8996666a5f27fb
Xiomara Gamez <xiomaragamez2003@gmail.com>
2025-08-02 12:12:44 -0700

    copy from SCRUM-41

---
+++ src/main/java/Login.java
@@ -0,0 +1,56 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Login {
+    private String username;
+    private String password;
+    private int hash;
+
+    public Login(String s1, String s2) {
+        this.username = s1;
+        this.password = s2;
+    }
+
+    public void setUsername(String u) {
+        this.username = u;
+    }
+
+    public void setPassword(String p) {
+        this.password = p;
+    }
+
+    public int getHashcode() {
+        return this.password.hashCode();
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getUserId() {
+        try(Connection con = DriverManager.getConnection("jdbc:sqlite:hotel.db")) {
+
+            try(PreparedStatement prst = con.prepareStatement("SELECT * FROM login WHERE username = ?")) {
+                prst.setString(1, this.getUsername());
+
+                try(ResultSet rs = prst.executeQuery()) {
+                    if(rs.next()) {
+                        return rs.getString(2);
+                    }
+                }
+            }
+        }
+        catch(SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+}
